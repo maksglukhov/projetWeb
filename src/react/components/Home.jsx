480ca0658c8713376bfcf00da5d5a70d7829aebe
sortique <75546265+sortique@users.noreply.github.com>
2022-05-29 15:47:04 +0200

    Update Home.jsx

--- src/react/components/Home.jsx
+++ src/react/components/Home.jsx
@@ -29,8 +29,63 @@ function Home({ admin, setIsLogged, setIsAdmin }) {
   }, []);
   console.log("is admin", admin);
   if (admin) {
-    return <div className='text-center'>You are admin</div>;
-  } else return <div className='text-center'>Hello world</div>;
+    return  <div className='text-center'> 
+    <br/>
+    <h4>En tant qu'admin, vous pouvez :</h4> 
+    <br/>
+        <d>
+        - Voir la listes des personnes avec leur statut, et si elles sont connectées, vous pouvez les déconnecter.
+        </d>
+        <br/><br/>
+        <d>
+        - Ajouter des évènements et des manches d'évènements, et vous pouvez supprimer un évènement
+        </d>
+        <br/><br/>
+        <d>
+        - Vous pouvez inscrire un autre user à une manche d'évènement
+        </d>
+        <br/><br/>
+        <d>
+         - Vous pouvez vous déconnecter
+        </d>
+        </div>
+        
+  }
+   
+   else
+  {
+    return <div className='text-center'>
+      <br/>
+      <h4>En tant qu'utilisateur non-connecté, vous pouvez : </h4>
+      <br/>
+        <d>
+        - Créer un compte user
+        </d>
+        <br/><br/>
+        <d>
+        - Vous connecter
+        </d>
+        <br/><br/>
+        <d>
+        - Consulter les évènements
+        </d>
+        <br/><br/>
+        <h4>En tant qu'utilisateur connecté, vous pouvez :</h4> 
+    <br/>
+        <d>
+        - Vous inscrire à des manches d'évènements
+        </d>
+        <br/><br/>
+        <d>
+          - Vous pouvez voir vos inscriptions
+        </d>
+        <br/><br/>
+        <d>
+          - Vous pouvez vous déconnecter
+        </d>
+        
+      </div>;
+  }
 }
 
 export default Home;
